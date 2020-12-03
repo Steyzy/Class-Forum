6979778cfe073bdb4471f2201b09b480da21edcb
Jing <zoujing.chloe@gmail.com>
2020-12-03 17:11:54 +0800

    updated profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react"
 import Navbar from '../components/Navbar';
 import ChangeName from "../components/Profile/ChangeName";
+import ChangeNationality from "../components/Profile/ChangeNationality";
+import ChangeYear from "../components/Profile/ChangeYear";
+
+
 
 export default class Profile extends Component {
     constructor(props) {
@@ -15,6 +19,8 @@ export default class Profile extends Component {
                 <h1>Profile</h1>
                 <Navbar loggedIn={true} />
                 <ChangeName />
+                <ChangeNationality />
+                <ChangeYear />
             </div>
         )
     }
