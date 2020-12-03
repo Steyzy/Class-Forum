ba4331bb3ac7bf16f7857a9aa58fea87aa107a41
Steven Yang <yangziyi_2018@163.com>
2020-12-03 11:26:12 +0800

    Add some basic styling to the home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,25 @@
 import React, { Component } from "react"
 import { Link } from 'react-router-dom';
 import { logout } from "../helpers/auth"
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: white;
+  color: palevioletred;
+  font-size: 1.1em;
+  margin: 1.5em;
+  padding: 0.5em 1.25em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  text-decoration: none;
+`
+
+const Wrapper = styled.section`
+  padding: 5em;
+  background: papayawhip;
+  text-align: center;
+  margin: 0;
+`
 
 export default class Navbar extends Component {
     constructor(props) {
@@ -14,17 +33,23 @@ export default class Navbar extends Component {
         return (
             <div className="navbar">
                 {this.state.loggedIn ? 
-                    <div>
-                        <Link to="/forum">Forum</Link> <br/>                 
+                    <Wrapper>
+                        <Button as="a" href="/forum">Forum</Button>
+                        <Button >Profile</Button>
+                        <Button onClick={logout}>Log Out</Button>
+                        {/*<Link to="/forum">Forum</Link> <br/>             
                         <Link to="/profile">Profile</Link> <br/>                 
-                        <button onClick={logout}>Log Out</button>
-                    </div> 
+                        <button onClick={logout}>Log Out</button>*/}
+                    </Wrapper> 
                  : 
-                    <div>
-                        <Link to="/">Home</Link> <br/>
+                    <Wrapper>
+                        <Button as="a" href="/">Home</Button>
+                        <Button as="a" href="/signup">Sign Up</Button>
+                        <Button as="a" href="/login">Login</Button>
+                        {/*<Link to="/">Home</Link> <br/>
                         <Link to="/signup">Sign Up</Link> <br/>               
-                        <Link to="/login">Login</Link>
-                    </div>
+                        <Link to="/login">Login</Link>*/}
+                    </Wrapper>
                 }
             </div>
         )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import Navbar from '../components/Navbar';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
+const Title = styled.h1`
+  font-size: 3em;
+  text-align: center; 
+  background: #d9f2ff;
+  padding: 1em;
+  margin: 0;
+  color: deepgrey;
+`
+const Words = styled.p`
+  text-align: center;
+  font-size: 1.1em;
+`
 export default class HomePage extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to Forum</h1>
+        <Title>Welcome to Forum</Title>
         <Navbar loggedIn={false} />
-        <p>Find your classmates here!</p>
+        <Words>Find your classmates here!</Words>
       </div>
     )
   }
