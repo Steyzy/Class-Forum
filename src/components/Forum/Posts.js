4437ecae7a6c4a2441b6688de0337db3df184afa
Ray Guo <siruiguo@outlook.com>
2020-12-08 19:08:07 +0000

    organize code in the Posts.js file

--- src/components/Forum/Posts.js
+++ src/components/Forum/Posts.js
@@ -156,83 +156,82 @@ export default class Posts extends Component {
     render(){
         return (
             <div id="myPost" className='left' style={{backgroundColor: "papayawhip"}}>
-            <button class="btn dropdown" data-toggle="collapse" data-target="#search">
-                <i class="icon-chevron-right"></i> Search for Post
-            </button>
-            <button class="btn dropdown" data-toggle="collapse" data-target="#write">
-                <i class="icon-chevron-right"></i> Write a Post
-            </button>
+                <button class="btn dropdown" data-toggle="collapse" data-target="#search">
+                    <i class="icon-chevron-right"></i> Search for Post
+                </button>
+                <button class="btn dropdown" data-toggle="collapse" data-target="#write">
+                    <i class="icon-chevron-right"></i> Write a Post
+                </button>
 
-            <div class="container" style={{textAlign: "center"}}>
-                <div class="collapse" id="search"  data-parent="#myPost">
-                    <div class="form-group p-3">
-                    <h5>Search for post</h5>
-                    <select onChange={this.handleChangeSearchOption} style={{marginBottom: "15px"}}>
-                        <option defaultValue="" disabled selected hidden>Select way of searching</option>
-                        <option>Post Name</option>
-                        <option>Content</option>
-                        <option>User Name</option>
-                    </select>                    
-                    <h5 for="keyword">Type the keyword</h5>
-                    <input id="keyword" value={this.state.SearchInput}
+                <div class="container" style={{textAlign: "center"}}>
+                    <div class="collapse" id="search"  data-parent="#myPost">
+                        <div class="form-group p-3">
+                            <h5>Search for post</h5>
+                            <select onChange={this.handleChangeSearchOption} style={{marginBottom: "15px"}}>
+                                <option defaultValue="" disabled selected hidden>Select way of searching</option>
+                                <option>Post Name</option>
+                                <option>Content</option>
+                                <option>User Name</option>
+                            </select>                    
+                            <h5 for="keyword">Type the keyword</h5>
+                            <input id="keyword" value={this.state.SearchInput}
                                         onChange={this.handleChangeSearchContent}></input>
-                    </div>
-                    <div id="accordion" class="pb-3">
-                    {this.state.filteredPosts.map(post => {
-                        return (
+                        </div>
+                        <div id="accordion" class="pb-3">
+                            {this.state.filteredPosts.map(post => {
+                            return (
                             <div class="card">
-                            <div class="card-header">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-8">
-                                    <a class="card-link" data-toggle="collapse" href="#post" 
-                                        key={post.id} style={{fontSize: "18px"}}>
-                                        {post.name}
-                                    </a>
+                                <div class="card-header">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-8">
+                                            <a class="card-link" data-toggle="collapse" href="#post" 
+                                                key={post.id} style={{fontSize: "18px"}}>
+                                                {post.name}
+                                            </a>
+                                        </div>
+                                        <div class="col-4" style={{textAlign: "right"}}>
+                                            { ` by `}
+                                            <Link to={{pathname: `/profile/${post.uid}`}}>{post.poster}</Link>
+                                            {"      "}
+                                            <div class="btn-group">
+                                                <button type="button" class="btn btn-primary" 
+                                                        href="#" name={post.id} onClick={this.handlePostSwitch}>
+                                                    View
+                                                </button>
+                                                <button type="button" class="btn btn-secondary" 
+                                                        href="#" name={post.uid} id={post.id}onClick={this.handleDel}>
+                                                    Delete
+                                                </button> 
+                                            </div>
+                                        </div>                                    
                                     </div>
-                                    <div class="col-4" style={{textAlign: "right"}}>
-                                    { ` by `}
-                                    <Link to={{pathname: `/profile/${post.uid}`}}>{post.poster}</Link>
-                                    {"      "}
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-primary" 
-                                                href="#" name={post.id} onClick={this.handlePostSwitch}>
-                                            View
-                                        </button>
-                                        <button type="button" class="btn btn-secondary" 
-                                                href="#" name={post.uid} id={post.id}onClick={this.handleDel}>
-                                            Delete
-                                        </button> 
+                                </div>
+                                </div>
+                                <div id="post" class="collapse" data-parent="#accordion">
+                                    <div class="card-body" style={{textAlign: "left"}}>
+                                        {post.content}
                                     </div>
                                 </div>
                             </div>
+                            )})} 
+                        </div>                            
+                    </div>
+                    <div class="collapse ml-5 mr-5 pl-5 pr-5" id="write"  data-parent="#myPost">
+                        <form onSubmit={this.handlePost}>
+                            <div class="form-group">
+                                <h5 for="title">Title</h5>
+                                <input type="text" id="title" value = {this.state.postName} 
+                                            onChange={this.handleChangePostName}  />
                             </div>
+                            <div class="form-group">
+                                <h5 for="content">Post Content</h5>
+                                <textarea class="form-control" rows="5" id="content" value={this.state.postContent}
+                                            onChange={this.handleChangePostContent}/>
+                                <button type="submit" class="button">Post</button>   
                             </div>
-                            <div id="post" class="collapse" data-parent="#accordion">
-                            <div class="card-body" style={{textAlign: "left"}}>
-                               {post.content}
-                            </div>
-                            </div>
-                        </div>
-                        )
-                    })} </div>
-                            
-                </div>
-                <div class="collapse ml-5 mr-5 pl-5 pr-5" id="write"  data-parent="#myPost">
-                    <form onSubmit={this.handlePost}>
-                        <div class="form-group">
-                            <h5 for="title">Title</h5>
-                            <input type="text" id="title" value = {this.state.postName} 
-                                        onChange={this.handleChangePostName}  />
-                        </div>
-                        <div class="form-group">
-                            <h5 for="content">Post Content</h5>
-                            <textarea class="form-control" rows="5" id="content" value={this.state.postContent}
-                                        onChange={this.handleChangePostContent}/>
-                            <button type="submit" class="button">Post</button>   
-                        </div>
-                    </form>            
-                </div>
+                        </form>            
+                    </div>
                 </div>
             </div>   
         )
