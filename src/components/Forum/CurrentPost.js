5f4aebca09140f9b17a316d90cb83abd2edb33b4
Ray Guo <siruiguo@outlook.com>
2020-12-20 16:22:25 +0000

    Add title under "Current Post"
    Display the post title under "Current Post" in the
    current post box in the forum page

--- src/components/Forum/CurrentPost.js
+++ src/components/Forum/CurrentPost.js
@@ -8,22 +8,35 @@ export default class CurrentPost extends Component {
         super(props);
         this.state = {
             currPostId: this.props.currPostId,
+            currPostTitle: '',
         }
 
     }
-    
+    // function name identified by React, do not change
     componentDidMount(){
-      this.setState({ currPostId: this.props.currPostId})
+        this.setState({ currPostId: this.state.currPostId})
+        db.ref('allposts/' + this.props.currPostId).on('value', snapshot => {
+            if(snapshot.val() != null){
+                this.setState({ currPostTitle: snapshot.val().name })
+            }
+        })
     }
     // function name identified by React, do not change
     componentWillReceiveProps(props) {
         this.setState({ currPostId: props.currPostId})
-	    }
+        db.ref('allposts/' + props.currPostId).on('value', snapshot => {
+            if(snapshot.val() != null){
+                this.setState({ currPostTitle: snapshot.val().name })
+            }
+        })
+	}
     
     render() {
       return (
             <div style={{textAlign: "center"}}>
-                <h2>Current Post: {this.state.currPost}</h2>
+                <h2>Current Post:  <br />
+                    {this.state.currPostTitle}
+                </h2>
                 {this.state.currPostId == '' ?
                     <p>Choose a post to view</p>
                     :
